1caf4af87fb93058da95c729f9c4b79230843665
Daniel kjobech <dan@deepwatertechnologies.com>
2025-08-11 10:45:30 -0700

    Allow requests from localhost for health

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,9 +39,16 @@ Rails.application.configure do
      localhost,
    ]
 
+  # Skip host checking for health check endpoints
+  config.host_authorization = {
+    exclude: ->(request) {
+        request.path.start_with?("/rails/health")
+    }
+  }
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
-
+  //
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
